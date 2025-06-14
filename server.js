124ba1624a0d7914cd86940355125369d2e1a5e6
gayatriii0803 <sanjay@rapydlaunch.com>
2025-06-15 00:07:10 +0530

    done

--- server.js
+++ server.js
@@ -53,9 +53,9 @@ app.use((err, req, res, next) => {
 });
 
 // 404 handler
-app.use('*', (req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+// app.use('*', (req, res) => {
+//   res.status(404).json({ message: 'Route not found' });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
