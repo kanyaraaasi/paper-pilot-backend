ba7d72a6682ac3a5f838bdd5e63990a8eaa6f21e
SanjaySargam <sargamsanjaykumar@gmail.com>
2025-06-21 10:21:09 +0530

    fix

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Database connection
 const DB_NAME = 'dev'; // <-- your custom DB name
-mongoose.connect(`mongodb+srv://admin123:admin123@paperpilot.7xgba1u.mongodb.net/${DB_NAME}`, {
+mongoose.connect(`mongodb+srv://admin123:admin123@paperpilot.7xgba1u.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
